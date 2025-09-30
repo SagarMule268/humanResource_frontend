bad51b3b32b023ae3d147adb9f1ba05f35caed16
SagarMule268 <sagarrm268@gmail.com>
2025-09-30 19:55:59 +0530

    updated base url for backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:5000/api/' ; 
+const BASE_URL = 'https://hr-backend-5kyq.onrender.com/api/' ; 
 // Replace with your API base URL
 export default axios.create({
     baseURL:BASE_URL,
